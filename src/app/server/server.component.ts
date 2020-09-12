e4e392b86c88a1f9318f371ab62b3430be763aff
Tomxss <theunis1g1coetzee@gmail.com>
2020-09-12 11:08:50 +0100

    added create server button

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServerComponent implements OnInit {
   serverStatus: String = "offline";
-  serverId = 5;
+  serverId = 404;
 
   constructor() { }
 
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  creationStatus = "No server was created.";
 
-  constructor() { }
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000)
+  }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer() {
+    this.creationStatus = "Server was created!!";
   }
 
 }
